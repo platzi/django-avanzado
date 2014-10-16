24c58d81a195d17eb228614fb9d5c5603ff7f4a7
harveydf <h@mejorando.la>
2014-10-16 15:38:57 -0500

    Clase 2

--- artists/urls.py
+++ artists/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
+from artists.views import AlbumListView, AlbumDetailView
 
 
 urlpatterns = patterns('',
+    url(r'^albums/$', AlbumListView.as_view(), name='album_list'),
+    url(r'^albums/(?P<artist>[\w\-]+)/$', AlbumListView.as_view(), name='album_list'),
+    url(r'^albums/detail/(?P<slug>[\w\-]+)/$', AlbumDetailView.as_view(), name='album_detail'),
 )
---
+++ artists/views.py
@@ -0,0 +1,77 @@
+from django.http import JsonResponse
+from django.views.generic import ListView, DetailView
+from artists.models import Album
+
+
+class JsonResponseMixin(object):
+
+    def response_handler(self):
+        format = self.request.GET.get('format', None)
+        if format == 'json':
+            return self.json_to_response()
+
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def json_to_response(self):
+        data = self.get_data()
+        return JsonResponse(data, safe=False)
+
+
+class AlbumListView(JsonResponseMixin, ListView):
+    model = Album
+    template_name = 'album_list.html'
+    paginate_by = 2
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return self.response_handler()
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumListView, self).get_context_data(**kwargs)
+        page = self.request.GET.get('page')
+
+        context.update({'page': page})
+        return context
+
+    def get_data(self):
+        data = [{
+            'cover': album.cover.url,
+            'title': album.title,
+            'slug': album.slug,
+            'artist': album.artist.nickname,
+        } for album in self.object_list]
+
+        return data
+
+    def get_queryset(self):
+        if self.kwargs.get('artist'):
+            queryset = self.model.objects.filter(artist__slug__contains=self.kwargs['artist'])
+        else:
+            queryset = super(AlbumListView, self).get_queryset()
+
+        return queryset
+
+
+class AlbumDetailView(JsonResponseMixin, DetailView):
+    model = Album
+    template_name = 'album_detail.html'
+    # slug_url_kwarg = 'djangoavanzado'
+    # slug_field = 'title'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return self.response_handler()
+
+    def get_data(self):
+        data = {
+            'album':{
+                'cover': self.object.cover.url,
+                'title': self.object.title,
+                'slug': self.object.slug,
+                'artist': self.object.artist.nickname,
+                'tacks': [t.title for t in self.object.track_set.all()]
+            }
+        }
+
+        return data
